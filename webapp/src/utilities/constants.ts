b346292c9c9313f52172c30baa09fd30d3d2fd6d
Sutiksh Verma <verma.su@northeastern.edu>
2024-02-12 21:01:51 -0800

    Only backend code

--- webapp/src/utilities/constants.ts
+++
@@ -1,3 +0,0 @@
-export const SidebarNavItems = ["Home", "Items", "Stock", "Build", "Customers", "Sales Orders", "Suppliers", "Manufacturers", "Purchase Orders", "Reports"]
-
-export const SidebarNavSupportItems = ["Help", "Integrations", "Logout", "My Profile"]
