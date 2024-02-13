f63eda43e45576a20cfeb3b7f179bfa306fc5317
Sutiksh Verma <verma.su@northeastern.edu>
2024-02-12 21:04:38 -0800

    only frontend

--- server/items/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Item, Tag, Category, CustomUser
-
-admin.site.register(CustomUser)
-
-admin.site.register(Item)
-admin.site.register(Tag)
-admin.site.register(Category)
